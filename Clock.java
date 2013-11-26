0c61642b88900085740c1eff86ac54cb8340d39d
vinijosi <vini_josi@yahoo.com.br>
2013-11-26 20:38:48 -0200

    Create Clock.java
    Clock
    Serve para sincronizar o tempo no SO

---
+++ Clock.java
@@ -0,0 +1,10 @@
+package br.unb.nso.TrabalhoNSO;
+
+public class Clock {
+	
+	int relogio = 0; // Tempo total de Uso da CPU
+	int velocidade = 1000; // Tempo em milisegundos de cada iteracao da CPU 
+	public void incrementa(){
+		this.relogio++;
+	}
+}
