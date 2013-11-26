e475b9c2bffa7cc7571c5e4eff8d40715fb4b94c
vinijosi <vini_josi@yahoo.com.br>
2013-11-26 20:37:21 -0200

    Update Cpu.java

--- Cpu.java
+++ Cpu.java
@@ -1,38 +1,45 @@
-package trabalhoNSO; 
+package br.unb.nso.TrabalhoNSO;
 
-public class CPU { 
-      
-    int cont; 
-    int semaforo; 
-    int aux; 
-      
-    public void processar(Processo o, int cont){ 
-        aux = 1; 
-        System.out.printf("PID: %s \n",o.pid); 
-//      System.out.println("offset: 0 "); 
-        System.out.printf("blocks: %s \n", o.blocosMemoria); 
-        System.out.printf("priority: %s \n",o.prioridade); 
-        System.out.printf("time: %s \n",o.tempoExecucao); 
-        System.out.printf("printers: %s  \n",o.impressora); 
-        System.out.printf("scanners: %s \n",o.scanner); 
-        System.out.printf("modems: %s \n",o.modem); 
-        System.out.printf("drives: %s \n",o.disco); 
-        System.out.println(); 
-        System.out.println(); 
-        System.out.println(); 
-           
-           
-        System.out.printf("Process %s =>\n",o.pid); 
-        System.out.printf("P%s STARTED\n",o.pid); 
-          
-        while (aux<=o.tempoExecucao){             
-            System.out.printf("P%s instruction %s \n",o.pid,aux);
-            //Fazer um sleep?
-            aux++; 
-        } 
-          
-        System.out.printf("P%s return SIGINT\n\n",o.pid);        
-          
-    } 
-      
-} 
+public class CPU {
+	
+	
+	Clock cpuTime = new Clock();
+//	int cont;
+//	int semaforo;
+//	int aux;
+	Processo processoPreemptado;
+	
+	public void processar(Processo o) throws InterruptedException{
+
+//		aux = 1;
+		System.out.printf("PID: %s \n",o.pid);
+//		System.out.println("offset: 0 ");
+		System.out.printf("blocks: %s \n", o.blocosMemoria);
+		System.out.printf("priority: %s \n",o.prioridade);
+		System.out.printf("time: %s \n",o.tempoExecucao);
+		System.out.printf("printers: %s  \n",o.impressora);
+		System.out.printf("scanners: %s \n",o.scanner);
+		System.out.printf("modems: %s \n",o.modem);
+		System.out.printf("drives: %s \n",o.disco);
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		 
+		 
+		System.out.printf("Process %s =>\n",o.pid);
+		System.out.printf("P%s STARTED\n",o.pid);
+		
+		while (o.tempoNaCpu < o.tempoExecucao){			
+			System.out.printf("P%s instruction %s \n",o.pid,o.tempoNaCpu+1);
+			o.tempoNaCpu++;
+			
+			//TimeUnit.MILLISECONDS.sleep(100);
+			this.cpuTime.incrementa();
+			
+		}
+		
+		System.out.printf("P%s return SIGINT\n\n",o.pid);		
+		
+	}
+	
+}
