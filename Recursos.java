9e8a8a55cc1d1eead93ae598c395dcdbae318dd1
vinijosi <vini_josi@yahoo.com.br>
2013-11-27 12:04:50 -0200

    Create Recursos.java
    Recursos
    Criar os Metodos

---
+++ Recursos.java
@@ -0,0 +1,16 @@
+package br.unb.nso.TrabalhoNSO;
+
+public class Recursos {
+	
+	public int impressora;
+	public int scanner;
+	public int disco;
+	public int modem;
+	public void alocaRecursos(Processo processo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
