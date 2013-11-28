a08fb39321c816394fe7cdbfa7d2042ab7f3b11c
vinijosi <vini_josi@yahoo.com.br>
2013-11-28 20:52:28 -0200

    Update Processo.java

--- src/br/unb/nso/TrabalhoNSO/Processo.java
+++ src/br/unb/nso/TrabalhoNSO/Processo.java
@@ -1,68 +1,74 @@
-package br.unb.nso.TrabalhoNSO;
-
-public class Processo extends Exception implements Comparable<Processo> {
-
-	public int pid;
-	public int tempoInicializacao;
-	public int prioridade;
-	public int tempoExecucao;
-	public int blocosMemoria;
-	public int impressora;
-	public int scanner;
-	public int modem;
-	public int disco;
-	public int idade;
-	public int tempoNaCpu;
+	package br.unb.nso.TrabalhoNSO;
 	
-	public void insereDados(
-			Processo o,
-			int pid,
-			int tempoInicializacao, 
-			int prioridade, 
-			int tempoExecucao,
-			int blocosMemoria, 
-			int impressora, 
-			int scanner, 
-			int modem, 
-			int disco) {
-		o.pid = pid;
-		o.tempoInicializacao = tempoInicializacao;
-		o.prioridade = prioridade;
-		o.tempoExecucao = tempoExecucao;
-		o.blocosMemoria = blocosMemoria;
-		o.impressora = impressora;
-		o.scanner = scanner;
-		o.modem = modem;
-		o.disco = disco;
-		o.idade = 0;
-		o.tempoNaCpu = 0;
+	@SuppressWarnings("serial")
+	public class Processo extends Exception implements Comparable<Processo> {
+	
+		public int pid;
+		public int tempoInicializacao;
+		public int prioridade;
+		public int tempoExecucao;
+		public int blocosMemoria;
+		public int impressora;
+		public int scanner;
+		public int modem;
+		public int disco;
+		public int idade;
+		public int tempoNaCpu;
 		
-	}
-
-	@Override
-	public int compareTo(Processo o) {
-		if((this.tempoInicializacao - o.tempoInicializacao) != 0){
-			return this.tempoInicializacao - o.tempoInicializacao;
-		}
-		else {
-		return this.prioridade - o.prioridade;
-		}
-	}
-
-	public boolean recursosLivres() {	
-		try{
+		public void insereDados(
+				Processo o,
+				int pid,
+				int tempoInicializacao, 
+				int prioridade, 
+				int tempoExecucao,
+				int blocosMemoria, 
+				int impressora, 
+				int scanner, 
+				int modem, 
+				int disco) {
+			o.pid = pid;
+			o.tempoInicializacao = tempoInicializacao;
+			o.prioridade = prioridade;
+			o.tempoExecucao = tempoExecucao;
+			o.blocosMemoria = blocosMemoria;
+			o.impressora = impressora;
+			o.scanner = scanner;
+			o.modem = modem;
+			o.disco = disco;
+			o.idade = 0;
+			o.tempoNaCpu = 0;
 			
-		} catch (Exception e){
-			System.out.println("Não existem recursos Disponiveis");			
 		}
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	public void envelhece() {
-		this.idade++;
+		
+		/*
+		 * O metodo compareTo garante que os processos estarao ordenados por tempo de execucao 
+		 * e por prioridade 
+		 * */
+	
+		@Override
+		public int compareTo(Processo o) {
+			if((this.tempoInicializacao - o.tempoInicializacao) != 0){
+				return this.tempoInicializacao - o.tempoInicializacao;
+			}
+			else {
+			return this.prioridade - o.prioridade;
+			}
+		}
+	
+		public boolean recursosLivres() {	
+			try{
+				return true;
+			} catch (Exception e){
+				System.out.println("Não existem recursos Disponiveis");			
+			}
+			// TODO Auto-generated method stub
+			return true;
+		}
+	
+		public void envelhece() {
+			this.idade++;
+			
+		}
+	
 		
 	}
-
-	
-}
