29fcec84c626ce9b94da5e9699c9a76dbb2bdf8d
root <root@notebook.(none)>
2013-11-29 17:34:22 -0200

    Modificado para exibir pid o processo incluido como pronto

--- src/br/unb/nso/TrabalhoNSO/Escalonador.java
+++ src/br/unb/nso/TrabalhoNSO/Escalonador.java
@@ -29,7 +29,7 @@ public class Escalonador {
 	public void incluiComoPronto(Processo novoProcesso) {
 		
 		this.processosProntos.add(novoProcesso);
-		System.out.println("Incluido como Pronto\n");
+		System.out.printf("\nProcesso %s Incluido como Pronto\n", novoProcesso.pid);
 	}
 	
 	/*
