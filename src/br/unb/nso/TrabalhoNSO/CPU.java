218acdd8707347ceac9d76796c327fa1b937f67f
vinicius <vinicius@notebook.(none)>
2013-12-01 16:06:22 -0200

    CPU Tenta liberar memoria no da Execucao do PRocesso.

--- src/br/unb/nso/TrabalhoNSO/CPU.java
+++ src/br/unb/nso/TrabalhoNSO/CPU.java
@@ -1,19 +1,19 @@
 package br.unb.nso.TrabalhoNSO;
 
 public class CPU {
-	
-	
+
+
 	Clock cpuTime = new Clock();
-//	int cont;
-//	int semaforo;
-//	int aux;
+	//	int cont;
+	//	int semaforo;
+	//	int aux;
 	Processo processoPreemptado;
-	
+
 	public void processar(Processo o) throws InterruptedException{
 
-//		aux = 1;
+		//		aux = 1;
 		System.out.printf("PID: %s \n",o.pid);
-//		System.out.println("offset: 0 ");
+		//		System.out.println("offset: 0 ");
 		System.out.printf("blocks: %s \n", o.blocosMemoria);
 		System.out.printf("priority: %s \n",o.prioridade);
 		System.out.printf("time: %s \n",o.tempoExecucao);
@@ -24,25 +24,25 @@ public class CPU {
 		System.out.println();
 		System.out.println();
 		System.out.println();
-		 
-		 
+
+
 		System.out.printf("Process %s =>\n",o.pid);
 		System.out.printf("P%s STARTED\n",o.pid);
-		
+
 		while (o.tempoNaCpu < o.tempoExecucao){			
 			System.out.printf("P%s instruction %s \n",o.pid,o.tempoNaCpu+1);
 			o.tempoNaCpu++;
-			
+
 			//TimeUnit.MILLISECONDS.sleep(100);
 			this.cpuTime.incrementa();
-			
+
 		}
-		
+
 		System.out.printf("P%s return SIGINT\n\n",o.pid);
 		Recursos.liberaRecursos(o);
 		Memoria.liberaMemoria(o);
-		
+
 	}
 
-	
+
 }
